b9d72821700e5e830b8e95cecea549ebe648ef6c
jjmmarcos <javierbulevar8@gmail.com>
2023-03-28 19:49:03 +0200

    service for control the map

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,24 +1,30 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';1
-import { PlacesService } from '../../services/places.service';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Map, Popup, Marker } from 'mapbox-gl';
+import { MapService, PlacesService } from '../../services';
+
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.css']
 })
 export class MapViewComponent implements AfterViewInit {
-  @ViewChild('mapDiv')
-  mapDivElement!: ElementRef;
 
-  constructor(private placesService: PlacesService) {}
+  @ViewChild('mapDiv') 
+  mapDivElement!: ElementRef
+
+  constructor( 
+    private placesService: PlacesService,
+    private mapService: MapService
+  ) { }
 
   ngAfterViewInit(): void {
-    if(!this.placesService.useLocation) throw Error('No hay placesService.userLocation');    
+    if ( !this.placesService.useLocation ) throw Error('No hay placesService.userLocation');
+      
     const map = new Map({
-      container: this.mapDivElement.nativeElement, // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: this.placesService.useLocation, // starting position [lng, lat]
-      zoom: 14, // starting zoom
+      container: this.mapDivElement.nativeElement,
+      style: 'mapbox://styles/mapbox/light-v10', // style URL
+      center: this.placesService.useLocation,
+      zoom: 14,
     });
 
     const popup = new Popup()
@@ -28,9 +34,12 @@ export class MapViewComponent implements AfterViewInit {
       `);
 
     new Marker({ color: 'red' })
-      .setLngLat(this.placesService.useLocation)
-      .setPopup(popup)
-      .addTo(map)
+      .setLngLat( this.placesService.useLocation )
+      .setPopup( popup )
+      .addTo( map )
+
+    this.mapService.setMap( map );
+
   }
 
 }
