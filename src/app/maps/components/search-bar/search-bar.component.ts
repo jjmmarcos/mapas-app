42cd7bd619d826d57b256d190d1217b467ebc0fb
jjmmarcos <javierbulevar8@gmail.com>
2023-04-02 20:07:18 +0200

    http petition

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-search-bar',
@@ -9,12 +10,12 @@ export class SearchBarComponent {
 
   private debounceTimer?: NodeJS.Timeout;
 
-  constructor() { }
+  constructor(private placesService: PlacesService) { }
 
   onQueryChanged(query: string = ''): void {
     if(this.debounceTimer) clearTimeout(this.debounceTimer);
     this.debounceTimer = setTimeout(() => {
-      console.log('Mandar este query: ' + query);
-    }, 1000);
+      this.placesService.getPlacesByQuery(query);
+    }, 350);
   }
 }
--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,16 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
   public useLocation?: [number, number] | undefined;
+  public isLoadingPlaces: boolean = false;
+  public places: Feature[] = [];
 
   get isUserLocationReady(): boolean {
     return !!this.useLocation;
   }
 
-  constructor() { 
+  constructor(private http: HttpClient) { 
     this.getUserLocation();
   }
 
@@ -28,5 +32,17 @@ export class PlacesService {
         }
       )
     });
+  }
+
+  getPlacesByQuery(query: string) {
+    this.isLoadingPlaces = true;
+
+    this.http.get<PlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?limit=5&proximity=-73.98263294111646,40.751773262025125`)
+      .subscribe(resp => {
+        console.log(resp.features[0]);
+
+        this.isLoadingPlaces = false;
+        this.places = resp.features;
+      });      
   }
 }
